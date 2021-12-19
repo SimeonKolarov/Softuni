d6af10468a33284582cca686840b8c37022a2fe7
SimeonKolarov <96375820+SimeonKolarov@users.noreply.github.com>
2021-12-19 17:49:20 +0200

    Add files via upload

---
+++ C# Advanced/StackAndQueue/StackAndQueue02.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Advance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(", ");
+            Queue<string> collection = new Queue<string>(input);
+
+
+            while (collection.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    collection.Dequeue();
+                }
+
+
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", collection));
+                }
+
+                else
+                {
+                    int index = command.IndexOf(' ');
+                    string song = command.Substring(index + 1);
+
+                    if (collection.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+
+                    else
+                    {
+                        collection.Enqueue(song);
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
