23e87c5d2112d6360ee4a4565339b362e4dc6c80
SimeonKolarov <96375820+SimeonKolarov@users.noreply.github.com>
2021-12-20 10:04:29 +0200

    Add files via upload

---
+++ C# OOP/Models/Athletes/Boxer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public class Boxer : Athlete
+    {
+        public Boxer(string fullName, string motivation, int numberOfMedals) : base(fullName, motivation, 60, numberOfMedals)
+        {
+
+        }
+
+        public override void Exercise()
+        {
+            this.Stamina += 15;
+
+            if (this.Stamina > 100)
+            {
+                this.Stamina = 100;
+                throw new ArgumentException("Stamina cannot be exceed 100 points.");
+            }
+        }
+    }
+}
